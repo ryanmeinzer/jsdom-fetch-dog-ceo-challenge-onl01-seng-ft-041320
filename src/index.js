ad229a68ab3be0445fdb6fc59bc0c5e906db7b2e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-20 21:14:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+// Challenge 1 
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchImages()
+})
+
+function fetchImages() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (results) {
+            addImages(results)
+        })
+}
+
+function addImages(urls) {
+    const dogImageContainer = document.querySelector('#dog-image-container')
+    urls.message.forEach(imageURL => {
+        let newImg = document.createElement('img')
+        newImg.src = imageURL
+        dogImageContainer.appendChild(newImg)
+    })
+}
+
+// Challenge 2
+
+
+
+
